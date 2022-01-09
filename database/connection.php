366b6deebdb08df656dc8ef88f3101a1b60f13fc
Faiz Ahmed <31391440+faiz28@users.noreply.github.com>
2022-01-09 22:23:27 +0600

    Update connection.php

--- database/connection.php
+++ database/connection.php
@@ -5,9 +5,11 @@ function connect_db()
     $username = "hslrlryvwddmtp";
     $password = "2c1607fb8215ebfaf00cc3d7b88b6f53031586b99a341729fd575f771ccf67e8";
     $dbname = "d55qs3nc85dpum";
+    $port = ""
 
     try {
-        $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+        $dsn = "pgsql:host=$host.";port=".$port.";dbname=".$dbname.";user=".$username.";password=".$password.";";
+        $conn = new PDO($dsn, $username, $password);
         // set the PDO error mode to exception
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         // $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
