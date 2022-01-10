bfa7ff183ad9f11d6b4e74f8fb2cc2eeb5319053
Faiz Ahmed <31391440+faiz28@users.noreply.github.com>
2022-01-10 01:30:17 -0800

    Update connection.php

--- database/connection.php
+++ database/connection.php
@@ -1,14 +1,12 @@
 <?php
 function connect_db()
 {
-    $host = "ec2-18-204-170-75.compute-1.amazonaws.com";
-    $username = "brxkrfljnfasgs";
-    $password = "f48864252b8dbdfd9c1cb5db0b70db05f5bda709517920865ccaaef393d0001f";
-    $dbname = "d65duhs36gng5m";
-    $port = "5432"
-
+    $host = "127.0.0.1";
+    $username = "root";
+    $password = "";
+    $dbname = "bookstore";
     try {
-        $dsn ="pgsql:host=$host;port=$port;dbname=$dbname";
+        $dsn ="mysql:host=$host;dbname=$dbname";
         $conn = new PDO($dsn, $username, $password);
         // set the PDO error mode to exception
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
